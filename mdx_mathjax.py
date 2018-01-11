47d0423e612d944f8e1e3ddf6432a6075c04fc2e
LIU Yugang <liuyug@gmail.com>
2018-01-11 15:00:58 +0800

    fix

--- mdx_mathjax.py
+++ mdx_mathjax.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 # based on mitya57/python-markdown-math
 
+import re
+
 from markdown.inlinepatterns import Pattern
 from markdown.extensions import Extension
 from markdown.util import AtomicString, etree
@@ -20,11 +22,14 @@ class MathJaxExtension(Extension):
             return 'math/tex'
 
     def extendMarkdown(self, md, md_globals):
+        def _translate_escape(text):
+            return escape_character.sub(r'\1', text)
+
         def _wrap_node(node, preview_text, wrapper_tag):
             if not self.getConfig('add_preview'):
                 return node
             preview = etree.Element('span', {'class': 'MathJax_Preview'})
-            preview.text = AtomicString(preview_text.replace('//', ''))
+            preview.text = AtomicString(_translate_escape(preview_text))
             wrapper = etree.Element(wrapper_tag)
             wrapper.extend([preview, node])
             return wrapper
@@ -32,13 +37,13 @@ class MathJaxExtension(Extension):
         def handle_match_inline(m):
             node = etree.Element('script')
             node.set('type', self._get_content_type(m.group(2)))
-            node.text = AtomicString(m.group(3).replace('//', ''))
+            node.text = AtomicString(_translate_escape(m.group(3)))
             return _wrap_node(node, ''.join(m.group(2, 3, 4)), 'span')
 
         def handle_match(m):
             node = etree.Element('script')
             node.set('type', '%s; mode=display' % self._get_content_type(m.group(2)))
-            node.text = AtomicString(m.group(3).replace('//', ''))
+            node.text = AtomicString(_translate_escape(m.group(3)))
             return _wrap_node(node, ''.join(m.group(2, 3, 4)), 'div')
 
         inline_patterns = (
@@ -56,6 +61,7 @@ class MathJaxExtension(Extension):
         for i, pattern in enumerate(inline_patterns):
             pattern.handleMatch = handle_match_inline
             md.inlinePatterns.add('mathjax-inline-%d' % i, pattern, '<escape')
+        escape_character = re.compile(r'\\(.)')
 
 
 def makeExtension(*args, **kwargs):
